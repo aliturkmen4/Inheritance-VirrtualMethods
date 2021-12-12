e44e8c97fbb4058b48c0df9fe74101675adae7b6
Ali Türkmen <84051961+aliturkmen4@users.noreply.github.com>
2021-12-12 20:32:55 +0300

    Add files via upload

---
+++ Kalıtım_EnginDemirog/Kalıtım_EnginDemirog/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kalıtım_EnginDemirog
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person[] persons = new Person[3]
+            {
+                new Customer()
+                {
+                    FirstName="Ali"
+                },new Student()
+                {
+                    FirstName="Resul"
+                },new Person()
+                {
+                    FirstName="Emin"
+                }
+            };
+            foreach (var person in persons)
+            {
+                Console.WriteLine(person.FirstName);
+            }
+            Console.ReadLine();           
+        }
+    }
+    class Person
+    {
+        public int ID { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+    }
+
+    class Customer : Person
+    {
+        public string City { get; set; }
+    }
+
+    class Student : Person
+    {
+        public string Departmant { get; set; }
+    }
+}
---
+++ Kalıtım_EnginDemirog/VitualMethods/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VitualMethods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SqlServer sqlserver = new SqlServer();
+            sqlserver.Add();
+            MySql mysql = new MySql();
+            mysql.Add();
+            Console.ReadLine();
+        }
+    }
+    class Database
+    {
+        public virtual void Add() 
+        {
+            Console.WriteLine("Added by Default");
+        }
+        public virtual void Delete()
+        {
+            Console.WriteLine("Deleted by Default");
+        }
+    }
+    class SqlServer : Database
+    {
+        public override void Add()
+        {
+            Console.WriteLine("Added by Sql Code");
+            //base.Add();
+        }
+    }
+    class MySql : Database
+    {
+
+    }
+}
